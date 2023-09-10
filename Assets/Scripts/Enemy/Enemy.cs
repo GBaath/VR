118a99ec77000b664deb962ddd5a696420f7f90f
Llrac <112984468+Llrac@users.noreply.github.com>
2023-09-10 02:58:20 +0200

    enemies wait for other enemies to "finish the job"

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,30 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
     public Animator anim;
     public GameObject target;
 
-    public int turnSpeed = 10;
+    [SerializeReference] EnemyData enemyData;
 
-    public enum State
-    {
-        chase,
-        attack
-    }
-    public State currentState = State.chase;
+    [HideInInspector] public bool mayFight = false;
 
+    // Default EnemyData
     int maxHits = 2;
     int currentHits;
-    float movementSpeed = 1f;
+    float movementSpeed = 0.75f;
     float attackRange = 2;
+    float turnSpeed = 25;
 
     Vector3 lastPosition = Vector3.zero;
-    bool waitingToMove = false;
-
-    [SerializeReference] EnemyData enemyData;
+    const float checkWaitRate = 0.1f;
+    const float chaseTurnSpeedMultiplier = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -32,12 +29,34 @@ public class Enemy : MonoBehaviour
         maxHits = enemyData.maxHits;
         movementSpeed = enemyData.movementSpeed;
         attackRange = enemyData.attackRange;
-        Application.targetFrameRate = 90;
+        turnSpeed = enemyData.turnSpeed;
+
+        // Check whether or not other enemies are against the target at the same time
+        InvokeRepeating(nameof(WaitForOtherEnemies), 0, checkWaitRate);
+    }
+
+    private void WaitForOtherEnemies()
+    {
+        // Get enemies closest to target
+        float closestDistance = Mathf.Infinity;
+        foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+        {
+            if (Vector3.Distance(target.transform.position, enemy.transform.position) < closestDistance)
+            {
+                closestDistance = Vector3.Distance(target.transform.position, enemy.transform.position);
+                foreach (Enemy enemy2 in FindObjectsOfType<Enemy>())
+                {
+                    enemy2.mayFight = false;
+                }
+                enemy.mayFight = true;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Behaviour
         if (target && Vector3.Distance(transform.position, target.transform.position) >= attackRange)
         {
             Chase();
@@ -50,37 +69,47 @@ public class Enemy : MonoBehaviour
 
     private void Chase()
     {
-        // Debugging
         Debug.DrawLine(transform.position, target.transform.position, Color.yellow);
-        currentState = State.chase;
 
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("attacking"))
         {
             anim.SetBool("waitingToMove", true);
             return;
         }
-        transform.LookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z));
-        lastPosition = transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+
+        Vector3 direction = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * chaseTurnSpeedMultiplier * Time.deltaTime);
+
+        if (mayFight)
+        {
+            lastPosition = transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+
+            attackRange = enemyData.attackRange;
+
+            anim.SetFloat("movementSpeed", Vector3.Distance(lastPosition, transform.position) / Time.deltaTime);
+        }
+        else
+        {
+            // Waits for other enemies to "finish the job"
+            anim.SetFloat("movementSpeed", 0);
+        }
+
         anim.SetTrigger("chase");
-        anim.SetFloat("movementSpeed", Vector3.Distance(lastPosition, transform.position) / Time.deltaTime);
     }
 
     private void Attack()
     {
-        // Debugging
         Debug.DrawLine(transform.position, target.transform.position, Color.red);
-        currentState = State.attack;
 
-        anim.SetTrigger("attack");
-        anim.SetBool("waitingToMove", false);
-
-        Vector3 direction = target.transform.position - transform.position;
+        Vector3 direction = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
         Quaternion targetRotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
 
-        //transform.rotation = Quaternion.RotateTowards(transform.position, target.transform.position);
+        attackRange = enemyData.attackRange + 0.2f;
 
-        //Vector3.RotateTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime, movementSpeed * Time.deltaTime);
+        anim.SetTrigger("attack");
+        anim.SetBool("waitingToMove", false);
     }
 }
--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EnemyData : ScriptableObject
 {
     public int maxHits = 2;
-    public float movementSpeed = 2;
+    public float movementSpeed = 0.75f;
     public float attackRange = 2;
+    public float turnSpeed = 25;
 }
