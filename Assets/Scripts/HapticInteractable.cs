10c8a9f3be016f5e9d8de315d33bd0e8222f669c
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-09-26 17:38:04 +0200

    Revert "Revert "Haptic feedback should be back on wheels""
    This reverts commit 331dfd3000a037433063248b80d6f87ef89eb883.

--- Assets/Scripts/HapticInteractable.cs
+++ Assets/Scripts/HapticInteractable.cs
@@ -5,20 +5,25 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HapticInteractable : MonoBehaviour
 {
+    public string Spotinditactor;
     [Range(0, 1)]
     public float intensity;
     public float duration;
     XRBaseControllerInteractor baseController;
-
+    public bool shouldNotAutoListen;
     void Start()
     {
         XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        if (!shouldNotAutoListen)
+        {
         if (interactable != null)
         {
             interactable.activated.AddListener(SetController);
             //interactable.deactivated.AddListener(RemoveController);
             interactable.hoverEntered.AddListener(HoverEnter);
             //interactable.hoverExited.AddListener(HoverExit);
+        }
+
         }
     }
 
