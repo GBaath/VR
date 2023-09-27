c1be1e6b26e1f565f9b735ee7fd82acc3cfd2144
GVBååth <gustav.baath02@gmail.com>
2023-09-27 11:08:55 +0200

    add pitch range param for soundplayer

--- Assets/Scripts/Utilities/AnimSoundPlayer.cs
+++ Assets/Scripts/Utilities/AnimSoundPlayer.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class AnimSoundPlayer : MonoBehaviour
 {
     public AudioClip clip1, clip2, clip3;
+    public float pitchRange = 0;
+    public float volume = 1;
 
     //for use with animatorevents
     public void PlayClip1()
     {
-        AudioSource.PlayClipAtPoint(clip1, transform.position);
+        CustomPlayClipAtPoint(clip1, transform.position,volume,Random.Range(-pitchRange,pitchRange));
     }
     public void PlayClip2()
     {
-        AudioSource.PlayClipAtPoint(clip2, transform.position);
+        CustomPlayClipAtPoint(clip2, transform.position,volume,Random.Range(-pitchRange,pitchRange));
     }
     public void PlayClip3()
     {
-        AudioSource.PlayClipAtPoint(clip3, transform.position);
+        CustomPlayClipAtPoint(clip3, transform.position,volume,Random.Range(-pitchRange,pitchRange));
+    }
+
+
+    public void CustomPlayClipAtPoint(AudioClip clip, Vector3 position, [UnityEngine.Internal.DefaultValue("1.0F")] float volume, float pitch)
+    {
+        GameObject gameObject = new GameObject("One shot audio");
+        gameObject.transform.position = position;
+        AudioSource audioSource = (AudioSource)gameObject.AddComponent(typeof(AudioSource));
+        audioSource.clip = clip;
+        audioSource.spatialBlend = 1f;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.Play();
+        Object.Destroy(gameObject, clip.length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
     }
 }
