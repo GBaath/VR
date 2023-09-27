9666ebaa80747c354c9dfdaf9fb918a6d8e4645c
Llrac <112984468+Llrac@users.noreply.github.com>
2023-09-27 15:09:44 +0200

    enemies attack using AnimationEvents instead

--- Assets/Scripts/Projectiles/ProjectileDamage.cs
+++ Assets/Scripts/Projectiles/ProjectileDamage.cs
@@ -1,43 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileDamage : MonoBehaviour
-{
+public class ProjectileDamage : MonoBehaviour {
     public int damage = 1;
+    public bool fromEnemy = false;
     public bool isHoming = false;
 
-    void Start()
-    {
+    void Start() {
         InvokeRepeating(nameof(HomeInOnEnemy), 0, 0.2f);
     }
 
-    private void HomeInOnEnemy()
-    {
-        if (!isHoming || !FindObjectOfType<Enemy>()) { return; }
+    private void HomeInOnEnemy() {
+        if (!isHoming) { return; }
 
         float closestDistance = Mathf.Infinity;
-        Transform closestEnemyTransform = null;
-        foreach (Enemy enemy in FindObjectsOfType<Enemy>())
-        {
-            if (Vector3.Distance(transform.position, enemy.transform.position) < closestDistance)
-            {
-                closestDistance = Vector3.Distance(transform.position, enemy.transform.position);
-                closestEnemyTransform = enemy.GetRandomLimb().transform;
+        Transform closestTargetTransform = null;
+        if (fromEnemy) {
+            closestTargetTransform = GameManager.instance.playerPhysicsBase.transform;
+        } else {
+            if (!FindAnyObjectByType<Enemy>()) { return; }
+            foreach (Enemy enemy in FindObjectsOfType<Enemy>()) {
+                if (Vector3.Distance(transform.position, enemy.transform.position) < closestDistance) {
+                    closestDistance = Vector3.Distance(transform.position, enemy.transform.position);
+                    closestTargetTransform = enemy.GetRandomLimb().transform;
+                }
             }
         }
-        transform.LookAt(new Vector3(closestEnemyTransform.position.x, closestEnemyTransform.position.y, closestEnemyTransform.position.z));
+        
+        transform.LookAt(new Vector3(closestTargetTransform.position.x, closestTargetTransform.position.y, closestTargetTransform.position.z));
         GetComponent<Rigidbody>().velocity = transform.forward * 10;
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
+    private void OnCollisionEnter(Collision other) {
         Debug.Log(other.gameObject);
-        if ( other.gameObject.TryGetComponent(out HealthProperty hp)) //other.gameObject.layer == LayerMask.NameToLayer("Enemies") &&
-        {
-            hp.LoseHealth(damage);
-            AudioSource.PlayClipAtPoint(GameManager.instance.audioManager.hitFeedback,Camera.main.transform.position,1);
-            Destroy(gameObject);
+        if (fromEnemy) {
+
+        } else {
+            if (other.gameObject.TryGetComponent(out HealthProperty hp)) {
+                hp.LoseHealth(damage);
+                AudioSource.PlayClipAtPoint(GameManager.instance.audioManager.hitFeedback, Camera.main.transform.position, 1);
+                Destroy(gameObject);
+            }
         }
     }
 }
