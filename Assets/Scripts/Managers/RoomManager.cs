ed950bf6fc9b7429e03fa4e6f0707ed16f050f14
GVBååth <gustav.baath02@gmail.com>
2023-10-04 11:42:08 +0200

    add condition for bossroom

--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
+    public int roomsToDragon = 10;
+
+
     [SerializeField]private List<GameObject> roomBaseList, roomExtensionList, corridorList;
+    [SerializeField] GameObject dragonRoom;
     public GameObject roomEntrance;
     public bool nextIsCorridor;
 
-    public int roomsPassed;
+    public int roomsPassed; 
+
     
     public GameObject GetNewRoomBase()
     {
+        if(roomsPassed == roomsToDragon)
+        {
+            return dragonRoom;
+        }
         return roomBaseList[Random.Range(0,roomBaseList.Count)];
     }
     public GameObject GetNewCorridor()
